ed1de540dbd0d4ffe7e3ca912bd37ffc5107f8f4
llilicheng4 <llilicheng4@gmail.com>
2020-05-03 18:16:38 +0800

    Completed logout button

--- app/src/main/java/com/example/budgetapplication/HomeActivity.java
+++ app/src/main/java/com/example/budgetapplication/HomeActivity.java
@@ -4,13 +4,23 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class HomeActivity extends AppCompatActivity {
 
     private BottomNavigationView bottomNavigationView;
+    FirebaseAuth mFireBaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,5 +53,11 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
         getSupportFragmentManager().beginTransaction().replace(R.id.container,new DashboardFragment()).commit();
+    }
+
+    public void LogOut(View view) {
+        FirebaseAuth.getInstance().signOut();
+        Intent intToMain = new Intent(HomeActivity.this,MainActivity.class);
+        startActivity(intToMain);
     }
 }
