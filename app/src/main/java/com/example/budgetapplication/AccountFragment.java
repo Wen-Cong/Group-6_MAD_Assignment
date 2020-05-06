b2c6a209c06feafe67e121d09655b50f0935e4ae
Wen Cong Yeo <s10194723@connect.np.edu.sg>
2020-05-06 10:58:53 +0800

    removed unnecesarry code

--- app/src/main/java/com/example/budgetapplication/AccountFragment.java
+++ app/src/main/java/com/example/budgetapplication/AccountFragment.java
@@ -28,14 +28,8 @@ import de.hdodenhof.circleimageview.CircleImageView;
  * create an instance of this fragment.
  */
 public class AccountFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String TAG = "Account Activity";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
     StorageReference storageReference;
     CircleImageView profileImage;
 
@@ -48,8 +42,6 @@ public class AccountFragment extends Fragment {
     public static AccountFragment newInstance(String param1, String param2) {
         AccountFragment fragment = new AccountFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -58,8 +50,6 @@ public class AccountFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
