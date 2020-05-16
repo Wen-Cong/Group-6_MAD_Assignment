3672d994f5bcf55451d9c7fb46909e2b2cbd2ba6
s10193293@connect.np.edu.sg <s10193293@connect.np.edu.sg>
2020-05-16 14:16:02 +0800

    Added listview and cardview in dashboard and transaction history

--- app/src/main/java/com/example/budgetapplication/TransactionsFragment.java
+++ app/src/main/java/com/example/budgetapplication/TransactionsFragment.java
@@ -2,18 +2,24 @@ package com.example.budgetapplication;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class TransactionsFragment extends Fragment {
-
+    ListView listView;
     public TransactionsFragment() {
         // Required empty public constructor
     }
@@ -24,5 +30,18 @@ public class TransactionsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_transactions, container, false);
+    }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        listView = (ListView)view.findViewById(R.id.listview);
+        ArrayList<String>arrayList = new ArrayList<>();
+        arrayList.add("a");
+        ArrayAdapter arrayAdapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1,arrayList);
+        listView.setAdapter(arrayAdapter);
+
+
     }
 }
